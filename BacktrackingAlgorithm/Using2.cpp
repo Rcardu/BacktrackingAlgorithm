97930e56ec38a190822b8efb8ec6330fbefc43e1
ricardo <gq1090523745@163.com>
2023-05-09 21:02:07 +0800

    组合总和II与III，回文子串个数，可能的IP地址，一个集合中的所有子集

--- BacktrackingAlgorithm/Using2.cpp
+++ BacktrackingAlgorithm/Using2.cpp
@@ -1,6 +1,9 @@
 /*
  * @Description: 
  */
+/*
+ * @Description: 
+ */
 #ifndef _BASESMOOTHINGALGORITHM_H_
 #define _BASESMOOTHINGALGORITHM_H_
 #include<iostream>
@@ -11,11 +14,15 @@
 #include"Combination2.h"
 #include"Combination3.h"
 #include"SplitPalindromicStrings.h"
+#include"ResetIPConfigNumbers.h"
+#include"Subset.h"
 using namespace std;
 
 void Combination2Format();//组合总和II
 void Combination3Format();//组合总和III
 void SplitPalinForamt();//判断回文字串的个数
+void ResetIPConfigNumberFormat();//可能的IP地址
+void SubsetFormat();//一个集合中的所有子集
 //组合总和II
 void Combination2Format(){
     int N,M;
@@ -48,7 +55,7 @@ void Combination3Format(){
         cout<<endl;
     }
 }
-
+//判断回文字串的个数
 void SplitPalinForamt(){
     string s;
     cin>>s;
@@ -58,6 +65,29 @@ void SplitPalinForamt(){
     for(int i=0;i<vec.size();i++){
         for(int j=0;j<vec[i].size();j++){
             cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+//可能的IP地址
+void ResetIPConfigNumberFormat(){
+    string s;
+    cin>>s;
+    ResetIPConfigNumbers obj;
+    vector<string>vec=obj.ResetIPConfigNumbers_feel(s);
+    for(int i=0;i<vec.size();i++)cout<<vec[i]<<endl;
+}
+//一个集合中的所有子集
+void SubsetFormat(){
+    int N;
+    cin>>N;
+    vector<int>nums(N);
+    for(int i=0;i<N;i++)cin>>nums[i];
+    Subset obj;
+    vector<vector<int>>vec=obj.Subset_feel(nums);
+    for(int i=0;i<vec.size();i++){
+        for(int j=0;j<vec[i].size();j++){
+            cout<<" { "<<vec[i][j]<<" } ";
         }
         cout<<endl;
     }
