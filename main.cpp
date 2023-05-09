a560d48acbba91412f29bb114b9e2830fa05c60a
ricardo <gq1090523745@163.com>
2023-05-09 14:47:29 +0800

    第1次提交

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ */
+#include<iostream>
+#include"BacktrackingAlgorithm//Using1.cpp"
+
+int main(){
+
+    //Combination_Format();
+    ThePhonenumberComFormat();
+
+    
+
+
+    while(getchar()!='\n')
+    continue;
+    getchar();
+    return 0;
+}
